d31ee1e3619f9bca9d2d86fd32cfb7007f488f61
Redas <Incaras@gmail.com>
2016-01-11 16:22:31 +0200

    add initial solution with two projects

---
+++ ForEach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEach
+{
+    class Program
+    {
+        public static int ListSize = 101;
+
+        static void Main()
+        {
+            Random random = new Random();
+            List<int> grades = new List<int>(ListSize);
+
+            while (grades.Count <= 100)
+            {
+                grades.Add(random.Next(ListSize));
+            }
+
+            List<int> filteredGrades = new List<int>();
+ 
+            foreach (int grade in grades)
+            {
+                if (grade >= 60)
+                {
+                    filteredGrades.Add(grade);
+                }
+            }
+          
+            Console.WriteLine("These are the {0} grades that passed the exam out of an overall of 101 students", filteredGrades.Count);
+
+            foreach (int grade in filteredGrades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
