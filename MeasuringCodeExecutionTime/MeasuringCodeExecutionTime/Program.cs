0b21a90c52ee087cdc1153e98307e31610b5879f
Redas <Incaras@gmail.com>
2016-03-10 15:42:53 +0200

    Add new exercise

---
+++ MeasuringCodeExecutionTime/MeasuringCodeExecutionTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasuringCodeExecutionTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> listRandomSymbols = new List<string>();
+            HashSet<string> hastSetRandomSymbols = new HashSet<string>();
+
+            string symbols = "abcdefghjjklmnobABHWDW)QIJD_QW_D)QW_D>>>>>DMDWQOJD4654891235489569845126598";
+            Random random = new Random();
+
+
+        }
+    }
+}
