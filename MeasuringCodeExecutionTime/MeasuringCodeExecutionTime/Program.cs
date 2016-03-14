6a563f8fe9689a8372f365adbaa77283a64c5430
Redas <Incaras@gmail.com>
2016-03-14 16:31:11 +0200

    Completed exercise

--- MeasuringCodeExecutionTime/MeasuringCodeExecutionTime/Program.cs
+++ MeasuringCodeExecutionTime/MeasuringCodeExecutionTime/Program.cs
@@ -1,21 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace MeasuringCodeExecutionTime
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Stopwatch stopwatch = new Stopwatch();
 
             List<string> listRandomSymbols = new List<string>();
             HashSet<string> hastSetRandomSymbols = new HashSet<string>();
 
             string symbols = "abcdefghjjklmnobABHWDW)QIJD_QW_D)QW_D>>>>>DMDWQOJD4654891235489569845126598";
             Random random = new Random();
+
+            if (listRandomSymbols.Count < 100000)
+            {
+                listRandomSymbols.Add(random.Next(symbols.Length).ToString());
+            }
+
+            stopwatch.Start();
+            listRandomSymbols.Contains("DMD");
+            Console.WriteLine("List search took " + stopwatch.Elapsed);
+            stopwatch.Stop();
+
+            if (hastSetRandomSymbols.Count < 100000)
+            {
+                hastSetRandomSymbols.Add(random.Next(symbols.Length).ToString());
+            }
+
+            stopwatch.Start();
+            listRandomSymbols.Contains("123");
+            Console.WriteLine("Hash Set search took " + stopwatch.Elapsed);
+            stopwatch.Stop();
 
 
         }
