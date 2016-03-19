4f6cee2fbce0a22f7471feee6e021df09c24323f
Redas <Incaras@gmail.com>
2016-03-19 13:25:10 +0200

    Update exercise

--- MeasuringMemoryUsage/Program.cs
+++ MeasuringMemoryUsage/Program.cs
@@ -4,55 +4,37 @@ using System.Diagnostics;
 
 namespace MeasuringMemoryUsage
 {
-    class Program
+    internal class Program
     {
         private static void Main()
         {
-            Process process = Process.GetCurrentProcess();
-            var workingSetBefore = process.WorkingSet64;
-            Console.WriteLine($"Before list: {workingSetBefore}");
-
             List<string> listRandomSymbols = new List<string>();
             HashSet<string> hastSetRandomSymbols = new HashSet<string>();
 
             string symbols = "abcdefghjjklmnobABHWDW)QIJD_QW_D)QW_D>>>>>DMDWQOJD4654891235489569845126598";
+
+            var beforeList = GC.GetTotalMemory(false);
 
             for (int i = 0; i < 1000000; i++)
             {
                 listRandomSymbols.Add(symbols + i);
             }
 
-            process = Process.GetCurrentProcess();
-            Console.WriteLine($"After list: {process.WorkingSet64}");
-            Console.WriteLine("Memory allocated: " + ((process.WorkingSet64 - workingSetBefore)/(1024*1024)) +
-                              " Megabytes");
+            var afterList = GC.GetTotalMemory(false);
 
-            listRandomSymbols.Clear();
+            Console.WriteLine($"Before list: {beforeList/(1024*1024)} Megabytes. After list: {afterList/(1024*1024)} Megabytes. List took up " + ((afterList-beforeList)/(1024*1024)) + " Megabytes of memory.");
             listRandomSymbols = null;
-            GC.Collect();
 
-            process = Process.GetCurrentProcess();
-            workingSetBefore = process.WorkingSet64;
-            Console.WriteLine($"Before HashSet: {workingSetBefore}");
+            var beforeHashSetMemory = GC.GetTotalMemory(false);
 
             for (int i = 0; i < 1000000; i++)
             {
                 hastSetRandomSymbols.Add(symbols + i);
             }
 
-            process = Process.GetCurrentProcess();
-            Console.WriteLine($"After HashSet: {process.WorkingSet64}");
-            Console.WriteLine("Memory allocated: " + ((process.WorkingSet64 - workingSetBefore)/(1024*1024)) +
-                              " Megabytes");
-        }
-
-
-        //memory.PagedSystemMemorySize64(listRandomSymbols);
-            /*var listMemory = Process.GetProcesses(listRandomSymbols.ToString());
-            var hashSetMemory = Process.GetProcesses(hastSetRandomSymbols.ToString());
-
-            Console.WriteLine(listMemory.ToString());
-            Console.WriteLine(hashSetMemory.ToString());*/
+            var afterHashSetMemory = GC.GetTotalMemory(true);
+            Console.WriteLine($"Before HashSet: {beforeHashSetMemory / (1024 * 1024)} Megabytes. After HashSet: {afterHashSetMemory / (1024 * 1024)} Megabytes. HashSet took up " + ((afterHashSetMemory - beforeHashSetMemory) / (1024 * 1024)) + " Megabytes of memory.");
         }
     }
+}
 
