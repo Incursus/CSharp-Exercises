729be075f93724941124d4c060a55706735c1eaa
Redas <Incaras@gmail.com>
2016-03-21 09:14:28 +0200

    Complete exercise

--- MeasuringMemoryUsage/Program.cs
+++ MeasuringMemoryUsage/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace MeasuringMemoryUsage
 {
@@ -13,27 +12,29 @@ namespace MeasuringMemoryUsage
 
             string symbols = "abcdefghjjklmnobABHWDW)QIJD_QW_D)QW_D>>>>>DMDWQOJD4654891235489569845126598";
 
-            var beforeList = GC.GetTotalMemory(false);
+            var beforeList = GC.GetTotalMemory(forceFullCollection:false);
 
             for (int i = 0; i < 1000000; i++)
             {
                 listRandomSymbols.Add(symbols + i);
             }
 
-            var afterList = GC.GetTotalMemory(false);
+            var afterList = GC.GetTotalMemory(forceFullCollection:false);
 
-            Console.WriteLine($"Before list: {beforeList/(1024*1024)} Megabytes. After list: {afterList/(1024*1024)} Megabytes. List took up " + ((afterList-beforeList)/(1024*1024)) + " Megabytes of memory.");
-            listRandomSymbols = null;
+            Console.WriteLine($"Before list: {beforeList/(1024*1024)} Megabytes. " +
+                              $"After list: {afterList/(1024*1024)} Megabytes. List took up " + ((afterList-beforeList)/(1024*1024)) + " Megabytes of memory.");
+            listRandomSymbols.Clear();
 
-            var beforeHashSetMemory = GC.GetTotalMemory(false);
+            var beforeHashSetMemory = GC.GetTotalMemory(forceFullCollection: true);
 
             for (int i = 0; i < 1000000; i++)
             {
                 hastSetRandomSymbols.Add(symbols + i);
             }
 
-            var afterHashSetMemory = GC.GetTotalMemory(true);
-            Console.WriteLine($"Before HashSet: {beforeHashSetMemory / (1024 * 1024)} Megabytes. After HashSet: {afterHashSetMemory / (1024 * 1024)} Megabytes. HashSet took up " + ((afterHashSetMemory - beforeHashSetMemory) / (1024 * 1024)) + " Megabytes of memory.");
+            var afterHashSetMemory = GC.GetTotalMemory(forceFullCollection:false);
+            Console.WriteLine($"Before HashSet: {beforeHashSetMemory / (1024 * 1024)} Megabytes. " +
+                              $"After HashSet: {afterHashSetMemory / (1024 * 1024)} Megabytes." + " HashSet took up " + ((afterHashSetMemory - beforeHashSetMemory) / (1024 * 1024)) + " Megabytes of memory.");
         }
     }
 }
