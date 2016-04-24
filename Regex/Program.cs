cae7eff0d4000b358c595869f890754c43420eac
Redas <Incaras@gmail.com>
2016-04-24 23:17:10 +0300

    Complete and update exercises

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace Regex
+namespace RegexMatching
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            //<p class="info">.*viewers paskutinis mūsų naudotas Regex
-            string twitchPageContent = File.ReadAllText("RegexExerciseData.txt");
+            //Used RegexBuddy for the exercise.
+            string twitchPageContent = File.ReadAllText("RegexExerciseData.htm");
+            var matchCollection = Regex.Matches(twitchPageContent, @"[0-9]+,[0-9]+\s+viewers");
+            List<int> viewerCounts = new List<int>();
+
+            foreach (object match in matchCollection)
+            {
+                var text = match.ToString().Replace("viewers", "").Replace(",", "").Trim();
+                int cleanCount = int.Parse(text);
+                viewerCounts.Add(cleanCount);
+            }
+
+            var moreThanThousandViewers = viewerCounts.Count(i => i > 1000);
+            var totalViewers = viewerCounts.Sum();
+            Console.WriteLine("Games that have over 1000 viewers: " + moreThanThousandViewers);
+            Console.WriteLine("Total viewer count: " + totalViewers);
         }
-
-
     }
 }
