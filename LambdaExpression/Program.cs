cb146900eee576e9fab22e995ff706c20183eaee
Redas <Incaras@gmail.com>
2016-05-03 19:27:33 +0300

    Add and complete exercise

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main()
+        {
+            //http://www.dotnetperls.com/lambda
+
+            char[] chars = "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&".ToCharArray();
+            Random r = new Random();
+            List<string> randomStrings = new List<string>();
+
+            for (int j = 0; j < 10000; j++)
+            {
+                int index1 = r.Next(chars.Length);
+                int index2 = r.Next(chars.Length);
+                int index3 = r.Next(chars.Length);
+                string randomString = $"{chars[index1]}{chars[index2]}{chars[index3]}";
+                randomStrings.Add(randomString);    
+            }
+
+            List<string> matchedStrings = randomStrings.FindAll(x => x.Contains("a"));
+            foreach (var matchedString in matchedStrings)
+            {
+                Console.WriteLine(matchedString);
+            }
+        }
+    }
+}
