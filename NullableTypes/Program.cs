58b91d19c441e77b794e9c5ac5af36131e798f15
Redas <Incaras@gmail.com>
2016-05-05 21:03:19 +0300

    Add and complete exercise

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int?> randomList = new List<int?>();
+            Random random = new Random();
+
+            List<int?> fixedValueList = new List<int?> {1, -1, null};
+
+            for (int i = 0; i < 10; i++)
+            {
+                var index = random.Next(0, 3);
+                var randomValue = fixedValueList[index];
+                randomList.Add(randomValue);
+            }
+
+            foreach (var element in randomList)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
