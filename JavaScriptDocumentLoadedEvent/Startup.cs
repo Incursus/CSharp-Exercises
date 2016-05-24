44babc9e68a6304e19e05631f4973ad1d322dad4
Redas <Incaras@gmail.com>
2016-05-24 18:09:53 +0300

    Add and update exercise

---
+++ JavaScriptDocumentLoadedEvent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JavaScriptDocumentLoadedEvent.Startup))]
+namespace JavaScriptDocumentLoadedEvent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
