14ab28c81d2c2e4fabceb3ab70dcae299ab0f36c
Redas <Incaras@gmail.com>
2016-06-04 09:20:38 +0300

    Add and update exercise

---
+++ ConcreteExceptionCatching/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConcreteExceptionCatching
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[10];
+            numbers[0] = 5;
+            numbers[1] = 6;
+            numbers[2] = 2;
+            numbers[3] = 3;
+            numbers[4] = 11;
+            numbers[5] = 12;
+            numbers[6] = 1;
+            numbers[7] = 0;
+            numbers[8] = 4;
+            numbers[9] = 7;
+
+            Console.WriteLine("Please choose a number from 0 to 9, and you'll get a corresponding number from the array.");
+
+            try
+            {
+                short userValue = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine($"The corresponding number in the array is {numbers[userValue]}");
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception);
+            }
+            catch (FormatException exception)
+            {
+                Console.WriteLine($"WE GOT AND ERROR!!! {exception}. Please try again.");
+            }
+        }
+    }
+}
