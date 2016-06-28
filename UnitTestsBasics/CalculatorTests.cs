f1d184cbe55be16efefb876301d111c6c27fd2bf
Redas <Incaras@gmail.com>
2016-06-28 19:00:55 +0300

    Update exercise

---
+++ UnitTestsBasics/CalculatorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestsBasics
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Init()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void MultiplyReturnsZeroIfSecondNumberIsZero()
+        {
+            var result = calculator.Multiply(5, 0);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void MultiplyReturnsPositiveNumberIfBothArgumentsArePositive()
+        {
+            var result = calculator.Multiply(5, 5);
+
+            Assert.AreEqual(25, result);
+        }
+
+        [Test]
+        public void DivideReturnsZeroIfFirstNumberIsZero()
+        {
+            var result = calculator.Divide(0, 5);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void DivideByZeroShouldThrow()
+        {
+            try
+            {
+                var result = calculator.Divide(5, 0);
+            }
+            catch (Exception e)
+            {
+                if (e is DivideByZeroException)
+                {
+                    return;
+                }
+            }
+            Assert.Fail("Expected DivideByZeroException");
+
+            //Assert.Throws(typeof(DivideByZeroException), () => calculator.DivideByZeroShouldThrow(5, 0));
+            
+        } 
+    }
+}
