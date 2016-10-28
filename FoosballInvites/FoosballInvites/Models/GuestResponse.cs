0261196315e34033dbe81ecacd1a66e8ef1ba6dc
Redas <Incaras@gmail.com>
2016-10-28 12:21:41 +0300

    Add initial exercise

---
+++ FoosballInvites/FoosballInvites/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoosballInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Įvesk vardą")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Parašyk, ar žaisi")]
+        public bool? WillAttend { get; set; }
+    }
+}
