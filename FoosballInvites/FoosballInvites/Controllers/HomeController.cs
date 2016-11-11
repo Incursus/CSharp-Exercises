24c15efae67f20bc5534b47048aeec8d508cc99e
Redas <Incaras@gmail.com>
2016-11-11 15:55:28 +0200

    Update exercise and fix errors

---
+++ FoosballInvites/FoosballInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using FoosballInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace FoosballInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Labas rytas" : "Laba diena";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+       
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+                return View("Ačiū", guestResponse);
+
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
