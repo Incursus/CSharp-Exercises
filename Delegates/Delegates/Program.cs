45e717b41dc511c84252e391d0d9abe7514d8fd6
Redas <Incaras@gmail.com>
2016-11-14 10:10:55 +0200

    Add and update exercise

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfNumbers = new List<int>();
+
+            for (int i = 0; i < 51; i++)
+            {
+                listOfNumbers.Add(i);
+            }
+
+            List<int> listOfEvenNumbers = new List<int>();
+            List<int> listOfOddNumbers = new List<int>();
+            List<int> listOfPrimeNumbers = new List<int>();
+            
+        }
+
+        private static Predicate<int> FilterByEvenNumber()
+        {
+            return delegate 
+        } 
+
+        public static bool IsEven(int value)
+        {
+            return value%2 == 0;
+        }
+    }
+}
