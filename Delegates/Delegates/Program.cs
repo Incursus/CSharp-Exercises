36b83bbb856c97f181db8140fcb863ec7ed762e8
Redas <Incaras@gmail.com>
2016-11-15 08:42:33 +0200

    Update exercise todo: refactor

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Delegates
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             List<int> listOfNumbers = new List<int>();
 
@@ -19,20 +14,71 @@ namespace Delegates
                 listOfNumbers.Add(i);
             }
 
-            List<int> listOfEvenNumbers = new List<int>();
-            List<int> listOfOddNumbers = new List<int>();
-            List<int> listOfPrimeNumbers = new List<int>();
-            
+            List<int> listOfEvenNumbers = listOfNumbers.FindAll(FilterByEvenNumber());
+            List<int> listOfOddNumbers = listOfNumbers.FindAll(FilterByOddNumber());
+            List<int> listOfPrimeNumbers = listOfNumbers.FindAll(FilterByPrimeNumber());
+
+            Console.WriteLine("Even numbers:");
+            foreach (int number in listOfEvenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Odd numbers:");
+            foreach (int number in listOfOddNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Prime numbers:");
+            foreach (int number in listOfPrimeNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
         }
 
         private static Predicate<int> FilterByEvenNumber()
         {
-            return delegate 
+            return delegate(int i) { return IsEven(i); };
+        }
+
+        private static Predicate<int> FilterByOddNumber()
+        {
+            return delegate(int i) { return IsOdd(i); };
+        }
+
+        private static Predicate<int> FilterByPrimeNumber()
+        {
+            return delegate(int i) { return IsPrime(i); };
         } 
 
         public static bool IsEven(int value)
         {
             return value%2 == 0;
+        }
+
+        public static bool IsOdd(int value)
+        {
+            return value%2 != 0;
+        }
+
+        public static bool IsPrime(int candidate)
+        {
+            // Test whether the parameter is a prime number.
+            if ((candidate & 1) == 0)
+            {
+                return candidate == 2;
+            }
+            for (int i = 3; (i*i) <= candidate; i += 2)
+            {
+                if ((candidate%i) == 0)
+                {
+                    return false;
+                }
+            }
+            return candidate != 1;
         }
     }
 }
