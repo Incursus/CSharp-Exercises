33d61b6c7e3e029d5cd954f66fff09a8c6dd3a69
Redas <Incaras@gmail.com>
2016-11-22 08:40:48 +0200

    Complete exercises

---
+++ MVCIntroduction/MVCIntroduction/Controllers/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCIntroduction.Models;
+
+namespace MVCIntroduction.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult DisplayStudentNames()
+        {
+            List<StudentModel> students = new List<StudentModel>();
+
+            students.Add(new StudentModel() {Name = "Redas Krik"});
+            students.Add(new StudentModel() {Name = "Darius Krik"});
+
+            return View(students);
+        }
+    }
+}
