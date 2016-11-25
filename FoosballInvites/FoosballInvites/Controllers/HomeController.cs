098f3c0446badbb336fc37a7f8fdc31fc068f561
Redas <Incaras@gmail.com>
2016-11-25 08:39:49 +0200

    Update exercise from C# PRO ASP.NET MVC 5

--- FoosballInvites/FoosballInvites/Controllers/HomeController.cs
+++ FoosballInvites/FoosballInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FoosballInvites.Models;
 
 namespace FoosballInvites.Controllers
 {
@@ -14,6 +15,19 @@ namespace FoosballInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good morning" : "Good day";
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(CollegueResponse collegueResponse)
+        {
+            // TODO: Email response to the party organizer
+            return View("Thanks", collegueResponse);
         }
     }
 }
---
+++ FoosballInvites/FoosballInvites/Models/CollegueResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoosballInvites.Models
+{
+    public class CollegueResponse
+    {
+        public string Name { get; set; }
+        public bool? WillPlay { get; set; }
+    }
+}
